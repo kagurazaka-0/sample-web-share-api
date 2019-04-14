e526f0ae34c052b7be44e3ed268a1fa3cfc913a2
katsuyaU <utsuhashi.can.do.it@icloud.com>
2019-04-14 18:16:36 +0900

    Add vue

---
+++ app/App.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+  .v-app
+    article(v-if="!isSupportedWebShareAPI")#danger.message.is-danger.my-not-show
+      .message-header
+        p
+          i.fas.fa-exclamation-circle
+          span Browser is not supported WebShareAPI.
+      .message-body このブラウザはWebShareAPIに対応してません。
+    .shares
+      .share(v-for="option of options")
+        pre
+          .title title
+          code.js.
+            {{ option | toStringFromObj }}
+        button(:disabled="!isSupportedWebShareAPI", @click="handleShareClick(option)").button.is-fullwidth.is-primary
+          span.icon.is-small
+            i.fas.fa-share-square
+          span 共有する
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+const isSupportedWebShareAPI = !!navigator.share
+
+const toStringFromObj = (obj: { [x: string]: any }) => {
+  let array: Array<string> = []
+  for (const key in obj) {
+    const value = obj[key]
+    const str = typeof value === "string" ? `"${value}"` : value
+    array.push(`\n    ${key}: ${str}`)
+  }
+  return `{${array.join(",")}\n}`
+}
+
+type Option = { url: string } | { text: string } | { title: string }
+
+@Component({
+  filters: {
+    toStringFromObj
+  }
+})
+export default class App extends Vue {
+  isSupportedWebShareAPI = isSupportedWebShareAPI
+
+  options: Array<Option> = [
+    { title: "Hello WebShareAPI!" },
+    { url: location.href },
+    { url: "https://qiita.com/katsuya_U" }
+  ]
+
+  handleShareClick(option: Option) {
+    navigator.share!(option)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-app {
+  width: 95vw;
+  max-width: 700px;
+  margin: auto;
+  padding: 20px;
+  border-radius: 7px;
+  background-color: #eeeeee;
+}
+
+.share {
+  margin-top: 15px;
+  pre {
+    position: relative;
+    .title {
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      background-color: gray;
+      padding: 0 10px;
+      font-size: 1rem;
+    }
+  }
+}
+
+p .fas ~ span,
+a ~ a {
+  margin-left: 10px;
+}
+</style>
--- app/main.ts
+++ app/main.ts
@@ -2,19 +2,14 @@ import "bulma"
 import "@fortawesome/fontawesome-free/css/all.css"
 ///@ts-ignore
 import hljs from "highlightjs"
+import Vue from "vue"
 
-const _ = document.querySelector.bind(document)
-const __ = document.querySelectorAll.bind(document)
-
-const isSupportedWebShareAPI = !!navigator.share
+import App from "./App.vue"
 
 document.addEventListener("DOMContentLoaded", e => {
   hljs.initHighlightingOnLoad()
 
-  // _(".my-content")!.innerHTML = "hello parcel!"
-
-  if (!isSupportedWebShareAPI) {
-    _("#danger")!.classList.remove("my-not-show")
-    __("button").forEach(el => (el.disabled = true))
-  }
+  new Vue({
+    render: h => h(App)
+  }).$mount("#app")
 })
